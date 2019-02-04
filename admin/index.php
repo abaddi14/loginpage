b7f4d85c8cf7f805b7d510303eb31d814b43b285
abaddi <abdullah-abduzeez@hotmail.com>
2019-02-04 07:10:40 -0500

    login page

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once('scripts/config.php')
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>weclome to your admin panel </title>
+</head>
+<body>
+  <h2> </h2>
+  <p> weclome username</P>
+
+    this admin page dashborads page
+<nav>
+  <ul>
+    <li> <a herf="#"> Create user </a> </li>
+    <li> <a herf="#"> Edit User  </a> </li>
+    <li> <a herf="#"> Delete User </a> </li>
+    <li> <a herf="scripts/caller.php?caller_id"> Sign out </a> </li>
+</body>
+</html>
---
+++ admin/scripts/config.php
@@ -0,0 +1,7 @@
+<?php
+require_once('function.php');
+require_once('session.php');
+require_once('read.php');
+require_once('login.php');
+
+ ?>
